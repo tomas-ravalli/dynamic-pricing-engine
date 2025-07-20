f6f64fff69ea41cde73890e8db771136c4860d30
Tomás Ravalli <tomravalli@gmail.com>
2025-07-21 00:04:51 +0200

    feat(decision_engine): create constants.py

---
+++ src/decision_engine/constants.py
@@ -0,0 +1,9 @@
+SAMPLE_BASE_FEATURES = {
+    'days_until_match': 15,
+    'opponent_strength': 4,
+    'match_importance': 5,
+    'month_of_match': 12,
+    'social_sentiment': 0.65,
+    'search_trends': 85,
+    'competing_events': 1
+}
